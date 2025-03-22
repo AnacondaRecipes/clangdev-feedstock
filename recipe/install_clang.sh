f0fdab5dda837c360f73248df091caf39dedde75
scott hain <shain@anaconda.com>
2025-03-22 08:19:36 -0700

    Fix up symlinks, cut down unneeded files included during install* scripts, fix linter issues, add linter overrides
    Signed-off-by: scott hain <shain@anaconda.com>

--- recipe/install_clang.sh
+++ recipe/install_clang.sh
@@ -3,3 +3,13 @@ set -ex
 
 cd ${SRC_DIR}/clang/build
 make install
+
+cd $PREFIX
+rm -rf lib/cmake include lib/lib*.a libexec share
+
+mv bin bin2
+mkdir -p bin
+
+MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
+mv ${PREFIX}/bin2/clang-${MAJOR_VERSION} ${PREFIX}/bin/clang-${MAJOR_VERSION}
+rm -rf bin2
--- recipe/install_clang_format.sh
+++ recipe/install_clang_format.sh
@@ -4,8 +4,15 @@ set -ex
 cd ${SRC_DIR}/clang/build
 make install
 
+cd $PREFIX
+rm -rf lib/cmake include lib/lib*.a libexec share
+
+mv bin bin2
+mkdir -p bin
+
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
-mv ${PREFIX}/bin/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
+mv ${PREFIX}/bin2/clang-format ${PREFIX}/bin/clang-format-${MAJOR_VERSION}
+rm -rf bin2
 
 if [[ "$PKG_NAME" == "clang-format" ]]; then
   ln -sf $PREFIX/bin/clang-format-${MAJOR_VERSION} $PREFIX/bin/clang-format
